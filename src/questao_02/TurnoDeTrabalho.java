c22aa6a3bc3a1c855e50b69fb262b210fadb18eb
Anderson-Batista <andersonbfs15@gmail.com>
2022-03-31 16:46:24 -0300

    Resolucao da questao 02

---
+++ src/questao_02/TurnoDeTrabalho.java
@@ -0,0 +1,31 @@
+package questao_02;
+
+import java.util.Scanner;
+
+public class TurnoDeTrabalho {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Em que turno você trabalha:\nM-matutino\nV-vespertino\nN-noturno");
+		System.out.println("OBS: digitar valores M, V ou N em maisculo.");
+		String turno = sc.next();
+		
+		switch (turno) {
+		case "M":
+			System.out.println("Bom dia!");
+			break;			
+		case "V":
+			System.out.println("Bom tarde!");
+			break;
+		case "N":
+			System.out.println("Bom noite!");
+			break;
+		default:
+			System.out.println("Valor inválido!");
+			break;
+		}
+		sc.close();
+	}
+}
