3e85f46dc9306bbc8f80bbbf4e18a7ab20b0896e
Anderson-Batista <andersonbfs15@gmail.com>
2022-03-31 17:27:32 -0300

    Resolucao da questao 03

--- src/questao_03/VendasDeSuco.java
+++ src/questao_03/VendasDeSuco.java
@@ -1,9 +1,48 @@
 package questao_03;
 
+import java.util.Scanner;
+
 public class VendasDeSuco {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Escolha o tipo de suco\n L-Limao \n T-tamarindo");
+		String tipoDeSuco = sc.next();
+		
+		if(tipoDeSuco.equals("L")) {
+			System.out.println("informe a quantidade de litros de suco de limão vendidos");
+			int qtdLitros = sc.nextInt();
+			
+			if(qtdLitros > 0 && qtdLitros <= 2) {
+				System.out.println("O cliente pagará 97% do valor total do suco.");
+			}
+			else if(qtdLitros > 2 && qtdLitros <= 5) {
+				System.out.println("O cliente pagará 100% do valor total do suco.");
+			}
+			else if(qtdLitros > 5) {
+				System.out.println("O cliente pagará 95% do valor total do suco.");
+			}
+		}
+		else if(tipoDeSuco.equals("T")) {
+			System.out.println("informe a quantidade de litros de suco de tamarindo vendidos");
+			int qtdLitros = sc.nextInt();
+			
+			if(qtdLitros > 0 && qtdLitros <= 2) {
+				System.out.println("O cliente pagará 96% do valor total do suco.");
+			}
+			else if(qtdLitros > 2 && qtdLitros <= 5) {
+				System.out.println("O cliente pagará 100% do valor total do suco.");
+			}
+			else if(qtdLitros > 5) {
+				System.out.println("O cliente pagará 94% do valor total do suco.");
+			}
+		}
+		else {
+			System.out.println("Digite uma valor valido L ou T");
+		}
+		sc.close();
 	}
 	
 }
